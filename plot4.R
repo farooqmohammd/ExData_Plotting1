064115f93555b0be38817c4ae6e4e6a67daeb538
farooqmohammd <68945747+farooqmohammd@users.noreply.github.com>
2020-08-31 22:28:14 +0530

    updated plot4.R

--- plot4.R
+++ plot4.R
@@ -15,8 +15,6 @@ summary(power)
 
 #calling png function and plotting the histogram
 png(file='plot4.png', width = 480,height = 480)
-hist(power$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
-dev.off()
 
 # initiating a composite plot 
 par(mfrow=c(2,2),mar=c(1,1,1,1))
@@ -31,3 +29,5 @@ with(power,lines(Time,Sub_metering_2,col="red"))
 with(power,lines(Time,Sub_metering_3,col="blue"))
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
 plot(power$Time,power$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
+
+dev.off()
